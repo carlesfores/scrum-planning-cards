fe1f8f4f88b992524145234424bb4e3ec61b23d3
Carles <fores.carles@gmail.com>
2026-09-09 17:03:06 +0200

    feat: card component rendering

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,31 @@
 import '@/components/Card.scss';
 
 const Card = ({ children, title, description }) => {
-  return(
-    <>
+
+  let showCardHeader = (title || description);
+  let cardTitle = null;
+  let cardDescription = null;
+
+  if (showCardHeader && title) {
+    cardTitle = <div className="card__title">{title}</div>;
+  }
+
+  if (showCardHeader && description) {
+    cardDescription = <div className="card__description">{description}</div>;
+  }
+
+  return (
       <div className="card">
-        <div className="card__header">
-          <div className="card__title">{ title }</div>
-          <div className="card__description">{ description }</div>
-        </div>
+        {(showCardHeader) && (
+          <div className="card__header">
+            {cardTitle}
+            {cardDescription}
+          </div>
+        )}
         <div className="card__content">
-          { children }
+          {children}
         </div>
       </div>
-    </>
   );
 }
 
