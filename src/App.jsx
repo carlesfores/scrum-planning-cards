b2b5cf6eb26b4c7cb5d2c9fe4655996a858a8805
Carles <fores.carles@gmail.com>
2026-09-09 14:53:01 +0200

    refactor: rename file types from css to scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.scss'
 
 function App() {
 
@@ -7,11 +7,18 @@ function App() {
       <div className='wrapper'>
 
         <header className='entity-header'>
-          <h1>Scrumm planning cards</h1>
+          <h1>Scrum planning cards</h1>
         </header>
 
         <main className='entity-content'>
-          <p>Main content</p>
+          <div>Main content </div>
+          <div className='entity-card'>
+            <div className='entity-card__title'>Card Title</div>
+            <div className='entity-card__description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+            <div className='entity-card__content'>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+          </div>
         </main>
 
       </div>
