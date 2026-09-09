2f7c03c1b014c2f8733418537526e74627b0e6fd
Carles <fores.carles@gmail.com>
2026-09-09 17:04:48 +0200

    feat: add css vars, update sandbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import './App.scss';
 import Card from '@/components/Card.jsx';
+import MainHeader from '@/components/MainHeader.jsx';
 
 function App() {
-
   return (
-    <>
-      <div className='wrapper'>
-        <header className='entity-header'>
-          <h1>Scrum planning cards</h1>
-        </header>
+      <div className='entity'>
+        <MainHeader></MainHeader>
+
         <main className='entity-content'>
-          <div>Main content </div>
-          <Card 
-            title="Card Title"
-            description="Lorem ipsum dolor sit amet"
-          >
+          <Card>
+            <p>Slot content</p>
+          </Card>
+          <Card title="Card Title" description="Lorem ipsum dolor sit amet">
+            <p>Slot content</p>
+          </Card>
+          <Card>
             <p>Slot content</p>
           </Card>
         </main>
+        
       </div>
-    </>
   )
-}
+};
 
 export default App
